5b56b8b37f40a5daacc51f3a47f649d83d7a8dc3
Renato <you@example.com>
2020-02-05 13:22:50 -0300

    criado o login controller

--- src/routes.js
+++ src/routes.js
@@ -5,5 +5,7 @@ const routes = Router()
 
 routes.post('/addrecipe', RecipesController.store)
 routes.get('/recipes', RecipesController.index)
-    
+
+routes.post('/login')
+
 module.exports = routes
