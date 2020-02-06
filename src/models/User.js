de135dee659504911ae24b0835a8620d5b6a7650
Renato <you@example.com>
2020-02-06 18:13:47 -0300

    arrumado o model de user

--- src/models/User.js
+++ src/models/User.js
@@ -5,6 +5,14 @@ const UserSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
     balance: {
         type: Number,
         required: true,
