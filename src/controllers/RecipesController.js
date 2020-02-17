d903c3533267d80f6a55010de64faec5565ab24e
Renato Filho <rrschiavofilho@gmail.com>
2020-02-16 23:36:21 -0300

    commit pra entender a budega amanha

--- src/controllers/RecipesController.js
+++ src/controllers/RecipesController.js
@@ -1,20 +1,47 @@
 const Recipe = require('../models/Recipe')
 const UserController = require('../controllers/UsersController')
+const User = require('../models/User')
 
 module.exports = {
     async store(req, res) {
         const { description, value } = req.body
-        const balance = UserController.getBalance()
 
         let recipe = await Recipe.create({
             description,
             value
         })
 
-        // await User.create({
-        //     balance
-        // })
-        return res.json(recipe)
+        let recipeValue = recipe.value
+        let user = await User.find()
+
+        if(user.balance == Number) {
+            const newValue = user.balance += recipeValue
+
+            console.log(user)
+            console.log(recipeValue)
+    
+            const balanceUpdate = await User.updateOne({ _id: user._id }, {
+                $set: {
+                    balance: newValue
+                }
+            })
+            return res.json(newValue)
+        } else {
+            const newValue = recipeValue
+
+            console.log(user)
+            console.log(recipeValue)
+    
+            const balanceUpdate = await User.updateOne({ _id: user._id }, {
+                $set: {
+                    balance: newValue
+                }
+            })
+            return res.json(newValue)
+        }
+
+
+        
     },
 
     async index(req, res) {
--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -7,16 +7,16 @@ module.exports = {
         if(recipes.length > 0) {
             try {
                 let balanceValue = 0
-                for(i = 0; i >= recipes.length; i++) {
-                    balanceValue += i.balance
+                for(i = 0; i <= recipes.length -1; i++) {
+                    balanceValue += recipes[i].value
                 }
+                
                 return res.json({ balance: balanceValue })
             } catch (error) {
                 return res.json({ message: error.message })
             }
         } else {
             return res.json({ balance: 0 })
-            console.log(recipes.length)
         }
 
         
